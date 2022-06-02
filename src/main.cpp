592ba6b0fb385037f457b78cc77f6b5d3292ef1c
Osmax-Br <91777959+Osmax-Br@users.noreply.github.com>
2022-06-02 21:29:15 +0300

    added platform io support

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,24 @@
+
 #include "ESPAsyncWebServer.h"
 #include <ArduinoJson.h>
 #include <HTTPClient.h>
+#include <Arduino.h>
 #include <ETH.h>
 #include <WiFi.h>
-//#include <WiFiAP.h>
+#include <WiFiAP.h>
 #include <WiFiClient.h>
-//#include <WiFiGeneric.h>
-//#include <WiFiMulti.h>
-//#include <WiFiScan.h>
+#include <WiFiGeneric.h>
+#include <WiFiMulti.h>
+#include <WiFiScan.h>
 #include <WiFiServer.h>
-//#include <WiFiSTA.h>
-//#include <WiFiType.h>
+#include <WiFiSTA.h>
+#include <WiFiType.h>
 #include <WiFiUdp.h>
 #include <Keypad.h>
 #include <NTPClient.h>
 #include <SPI.h>
 #include <Wire.h>
-//#include <WiFiUdp.h>
+#include <WiFiUdp.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "ThingSpeak.h"
@@ -45,29 +47,14 @@ IPAddress secondaryDNS(8, 8, 4, 4); //optional
 AsyncWebServer server(80);
 
 // Replaces placeholder with DHT values
-String processor(const String& var){
-  //Serial.println(var);
-  if(var == "Timer"){
-    return timer_var();
-  }
-  else if(var == "Chosen_server"){
-    return chosen_var();
-    }
-  else if(var == "Pause"){
-    return pause_var();
-    }  
-  return String();
-}
-
-String irkey = "";
-String irCode[4][2] = {{"202b04f","status"},{"2026897","pause"},{"202e817","resume"},{"20250af","upload"}};
 String serverName = "http://codeforces.com/api/contest.list";
 unsigned long lastTime_cf = 0;
-unsigned long timerDelay_cf = 200000;
 int temp_cf[11];
 int ContestTime [9] = {-1,-1,-1,-1,-1,-1,-1,-1,-1};
 String ContestDiv [9] = {"div -1","div -1","div -1","div -1","div -1","div -1","div -1","div -1","div -1"};
 String ContestPhase [9] = {"na","na","na","na","na","na","na","na","na"};
+String irkey = "";
+String irCode[4][2] = {{"202b04f","status"},{"2026897","pause"},{"202e817","resume"},{"20250af","upload"}};
 int table[12][2] = {{0, 0}, {1, 0}, {2, 0}, {0, 1}, {1, 1}, {2, 1}, {0,2}, {1,2}, {2,2}, {0,3}, {1,3}, {2,3}};
 const int activities = 4;
 String str[activities][13] = {{"school","mosque","sleep","musiq","eat","anime","bath","out","face","utube","quran","study","Nothing"},
@@ -119,116 +106,41 @@ String new_sec,new_minu,new_hor = "00";
 int saving = -1;
 String saved_data[20];
 const char* database_init = "CREATE table if not exists times(begin_hour int,  begin_minute int,  end_hour int,  end_minute int,  hours int,  minutes int,  seconds int,chosen string);";
-void IRAM_ATTR onTimer(){
-  if(startTimer == 1){
-  sec+=1;
-  if(sec>=59){
-    sec=0;
-    minu++;
-    }
-  if(minu>=59){
-    minu=0;
-    hor++;
-    } }
-   else if(startTimer ==0){
-   sec = 0;
-   hor =0;
-   minu = 0;
-   } 
-   beautiful_int(sec,hor,minu);
- } 
-void setup() {
-    Serial.begin(9600);
-    
-if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
-   
+
+
+String timer_var(){
+ return String(new_hor)+":"+String(new_minu)+":"+String(new_sec);
   }
-  int connection_begin = millis();
-     if(WiFi.status() != WL_CONNECTED){
-      while(WiFi.status() != WL_CONNECTED && millis() <= connection_begin+20000){
-        WiFi.begin(ssid, password);  
-        break;    
-      } 
-     
+String chosen_var(){
+    if(chosen==-1){
+      return "Nothing";
     }
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.clearDisplay();
-  display.setTextSize(0);
-  display.setTextColor(WHITE);
-  timeClient.begin();
-  timer = timerBegin(0, 80, true);  // timer 0, MWDT clock period = 12.5 ns * TIMGn_Tx_WDT_CLK_PRESCALE -> 12.5 ns * 80 -> 1000 ns = 1 us, countUp
-  timerAttachInterrupt(timer, &onTimer, true); // edge (not level) triggered 
-  timerAlarmWrite(timer, 1000000, true); // 1000000 * 1 us = 1 s, autoreload true
-  timerAlarmEnable(timer); // enable
-  ThingSpeak.begin(clientt);
-  web_server();
-  pinMode(19,OUTPUT); 
-  pinMode(27,OUTPUT);  
-  irrecv.enableIRIn();
-  irrecv.blink13(true);
-}
-void loop() {  
-char key = keypad.getKey();  
-if(key){
-  display.ssd1306_command(SSD1306_DISPLAYON);
-  lastPress = millis();
-  }  
-if(lastPress+30000<millis()){
-  display.ssd1306_command(SSD1306_DISPLAYOFF);
-  } 
-if(key == 'C'){
-  if(square==activities-1){
-      square = 0;}
   else{
-    square+=1;
+    return str[square][chosen] ;
+    }}
+String pause_var(){
+  if(startTimer==1){
+    return "Pause";
     }
-    if(pressA == 1){
-     display.clearDisplay();
-    print_label(square);
-    pressA = 1;
-    key = '?';
-    }}   
-if(key=='A' and pressA!=1 and pressD!=1) {
-  int lastSquare = square;
-  display.clearDisplay();
-  print_label(square);
-  pressA=1;
-  key = '?';
+  else if(startTimer == 2){
+    return "Resume";
+    }
+   else{
+    return "Start";
+    }}
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "Timer"){
+    return timer_var();
+  }
+  else if(var == "Chosen_server"){
+    return chosen_var();
+    }
+  else if(var == "Pause"){
+    return pause_var();
+    }  
+  return String();
 }
-else if(key=='A' and pressA==1){
-display.clearDisplay();
-display.display();
-pressA=0;}
-if(pressA==1){
-edit_key(key);
-if(key_num != -48){
-    chosen = key_num-1;
-  }} 
- if(key=='D' and pressD!=1 and pressA!=1) {
- display.clearDisplay();
-cf_div_screen("Div 3","12/june 05:02" , "rating: 837 || 5 left" , hor , minu , sec);
-display.display();
-pressD=1;
- } 
-else if(key=='D' and pressD==1){
-display.clearDisplay();
-display.display();
-pressD=0;
-
-} 
-if(pressA!=1 and pressD!=1){
-    main_screen(chosen,hor,minu,sec,key,new_hor,new_minu,new_sec);
-   drawer();  
- }
-get_ir_data();
-
-
-
-display.display();
-}
-
-
-
 void web_server(){
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/html", index_html, processor);
@@ -293,182 +205,27 @@ void web_server(){
   server.begin();
 }
 
-String timer_var(){
- return String(new_hor)+":"+String(new_minu)+":"+String(new_sec);
-  }
-String chosen_var(){
-    if(chosen==-1){
-      return "Nothing";
-    }
-  else{
-    return str[square][chosen] ;
-    }}
-String pause_var(){
-  if(startTimer==1){
-    return "Pause";
-    }
-  else if(startTimer == 2){
-    return "Resume";
-    }
-   else{
-    return "Start";
-    }}
+
+
 
 void drawer(){
   display.drawLine(0, 10, display.width() - 1, 10, WHITE);  
   }
 
 
-void main_screen(int chosen,int hor,int minu,int sec,char key,String new_hor,String new_minu,String new_sec){
- display.clearDisplay();
-   display.setCursor(0,40+15);
-  display.setTextSize(1);
-  display.print("choice: ");
-  if(chosen==-1){
-    display.print("Nothing");
-    }
-  else{
-    display.print(str[square][chosen]);
-    } 
- String ampm;
- if(WiFi.status() != WL_CONNECTION_LOST && WiFi.status()== WL_CONNECTED){ 
-  if(lastUpdate+120000<millis()){
-    cf_data();
- timeClient.update();
- lastUpdate = millis();
 
- }
- else if(millis()<10000){
-   cf_data();
-  timeClient.update();
-  }}
- formattedDate = timeClient.getFormattedDate();
- int splitT = formattedDate.indexOf("T");
- dayStamp = formattedDate.substring(5, splitT);
- hours=timeClient.getHours();
- day = daysOfTheWeek[timeClient.getDay()];
- minutes = timeClient.getMinutes();
- seconds = timeClient.getSeconds();
- year = formattedDate.substring(0,4);
- if(hours>12){
-  hours-=12;
-  ampm = "Pm";}
- else{
- ampm = "Am";
- }
- if(year == "1970"){
-  hours = minutes = seconds = 0;
-  day = "???";
-  dayStamp = "0-0";
-  }
-
- int clearr = millis();
- 
- 
- 
- display.setCursor(17,0+15);
- display.setTextSize(2);
- char bufffer[40];
- sprintf(bufffer, "%s:%s:%s",new_hor,new_minu,new_sec);
-
-if (key=='B' and pressB==0) {
-  start_hour = hours;
-  start_minute = minutes;
-  start_second = seconds;
-  counter_var=1;
-  pressB=1;
-  startTimer = 1;
-  key = '1';
-  pressed+=1;
-}
-else if(key=='B' and pressB==1){
-display.clearDisplay();
-display.display();
-pressB=0;
-counter_var=0;
-key = '1';
-}
-
-if(key=='*' and pressStar==0){
-display.clearDisplay();
-display.display();  
-pressStar=1;
-counter_var=2;
-key = '1';
-  }
-else if(key=='*' and pressStar==1){
-  pressStar=0;
-  counter_var=1;
-  }  
-if(counter_var==0){
-  if(pressed >= 0 and chosen!=-1){
-  display.clearDisplay();
-  display.print("Saving..");
-  display.display();
-  if ((millis() - lastTime) > timerDelay){
-    char datum[200];
-    sprintf(datum, "%s/%s/%i:%i:%i/%i:%i:%i/%i:%i:%i",str[square][chosen],dayStamp,start_hour,start_minute,start_second,new_hor,new_minu,new_sec,hours,minutes,seconds);
-    if(WiFi.status() != WL_CONNECTION_LOST && WiFi.status()== WL_CONNECTED){
-    int x = ThingSpeak.writeField(myChannelNumber, 1,datum, myWriteAPIKey);
-    
-    if(x == 200){
-       display.println("saved!"); 
-       delay(500);}
-    else{
-      
-    }}
-    else{
-      saving+=1;
-      char datum[200];
-      sprintf(datum, "%s/%s/%i:%i:%i/%i:%i:%i/%i:%i:%i",str[square][chosen],dayStamp,start_hour,start_minute,start_second,new_hor,new_minu,new_sec,hours,minutes,seconds);
-      saved_data[saving]=datum;
-      display.print("saved to var");
-      delay(500);
-      }}
-      lastTime = millis();
-     pressed = -1;
-      }
-  else{
-  display.print("00:00:00");}
-  startTimer = 0;
-
-}
-else if(counter_var==1){
-  display.print(bufffer);
-  startTimer = 1;
-}
-
-else if(counter_var==2){
-  display.print(bufffer);
-  startTimer = 2;
-}
-
- display.setTextSize(1);
- display.setCursor(0,20+15);
- char buffer[40];
- sprintf(buffer,"%i:%i:%i %s %s %s",hours,minutes,seconds,ampm,day,dayStamp);
- display.print(buffer);
- int savings = saving;
- if(saving>-1 and WiFi.status() != WL_CONNECTION_LOST && WiFi.status()== WL_CONNECTED){
-  display.print("saving");
-      for(int i=0;i<savings +1;){
-        if((millis() - lastTime ) > timerDelay){
-      int x = ThingSpeak.writeField(myChannelNumber,1,saved_data[i], myWriteAPIKey);
-      if(x==200){
-       i++;
-       saving -=1;
-       }
-       else{
-        }
-       lastTime = millis();
-       }
-      }
-      }
- 
-  }
         
 
-
+void draw_grid() {
+  display.drawLine(0, 0, 0, display.height() - 1, WHITE);
+  display.drawLine(41, 0, 41, display.height() - 1, WHITE);
+  display.drawLine(81, 0, 81, display.height() - 1, WHITE);
+  display.drawLine(127, 0, 127, display.height() - 1, WHITE);
+  for (int i = 0; i <= 64 - 15; i += 16) {
+    display.drawLine(0, i, display.width() - 1, i, WHITE);
+  }
+  display.drawLine(0, 63, display.width() - 1, 63, WHITE);}
+  
 void print_label(int square) {
   display.setTextSize(1);
   draw_grid();
@@ -480,16 +237,7 @@ void print_label(int square) {
   display.print(str[square][i]);
   }
 }
-void draw_grid() {
-  display.drawLine(0, 0, 0, display.height() - 1, WHITE);
-  display.drawLine(41, 0, 41, display.height() - 1, WHITE);
-  display.drawLine(81, 0, 81, display.height() - 1, WHITE);
-  display.drawLine(127, 0, 127, display.height() - 1, WHITE);
-  for (int i = 0; i <= 64 - 15; i += 16) {
-    display.drawLine(0, i, display.width() - 1, i, WHITE);
-  }
-  display.drawLine(0, 63, display.width() - 1, 63, WHITE);}
-  
+
 void keypress() {
 display.setCursor(0, 0);
 char key = keypad.getKey();
@@ -617,21 +365,6 @@ if (irrecv.decode(&results)){
 
   }
 
-void cf_div_screen(String upcoming,String contest_date,String ratingAndProblems , int hor , int minu , int sec){
-display.setCursor(30,1);
-display.setTextSize(2);
-display.print(upcoming);
-display.setTextSize(1);
-display.setCursor(20,20);
-display.print(contest_date);
-display.setTextSize(2);
-display.setCursor(10,35);
-display.print("00:00:--");
-display.setTextSize(1);
-display.setCursor(0,56);
-display.print("rating: 837 || x left");
-}  
-
 void cf_data(){
       HTTPClient httpc;
       // Send HTTP GET request
@@ -658,14 +391,301 @@ void cf_data(){
         }}
       // Free resources
       httpc.end();
-  for(int i = 0 ; i<9 ; i++){
-   Serial.print(ContestDiv[i]);
-   Serial.print(" ");
-   Serial.print(ContestPhase[i]);
-   Serial.print(" ");
-   Serial.print(ContestTime[i]);
-   Serial.println();
   }
-    
 
+void main_screen(int chosen,int hor,int minu,int sec,char key,String new_hor,String new_minu,String new_sec){
+ display.clearDisplay();
+   display.setCursor(0,40+15);
+  display.setTextSize(1);
+  display.print("choice: ");
+  if(chosen==-1){
+    display.print("Nothing");
+    }
+  else{
+    display.print(str[square][chosen]);
+    } 
+ String ampm;
+ formattedDate = timeClient.getFormattedDate();
+ int splitT = formattedDate.indexOf("T");
+ dayStamp = formattedDate.substring(5, splitT);
+ hours=timeClient.getHours();
+ day = daysOfTheWeek[timeClient.getDay()];
+ minutes = timeClient.getMinutes();
+ seconds = timeClient.getSeconds();
+ year = formattedDate.substring(0,4);
+ if(hours>12){
+  hours-=12;
+  ampm = "Pm";}
+ else{
+ ampm = "Am";
+ }
+ if(year == "1970"){
+  hours = minutes = seconds = 0;
+  day = "???";
+  dayStamp = "0-0";
+  }
+
+ int clearr = millis();
+ 
+ 
+ 
+ display.setCursor(17,0+15);
+ display.setTextSize(2);
+ char bufffer[40];
+ sprintf(bufffer, "%s:%s:%s",new_hor,new_minu,new_sec);
+
+if (key=='B' and pressB==0) {
+  start_hour = hours;
+  start_minute = minutes;
+  start_second = seconds;
+  counter_var=1;
+  pressB=1;
+  startTimer = 1;
+  key = '1';
+  pressed+=1;
+}
+else if(key=='B' and pressB==1){
+display.clearDisplay();
+display.display();
+pressB=0;
+counter_var=0;
+key = '1';
+}
+
+if(key=='*' and pressStar==0){
+display.clearDisplay();
+display.display();  
+pressStar=1;
+counter_var=2;
+key = '1';
+  }
+else if(key=='*' and pressStar==1){
+  pressStar=0;
+  counter_var=1;
+  }  
+if(counter_var==0){
+  if(pressed >= 0 and chosen!=-1){
+  display.clearDisplay();
+  display.print("Saving..");
+  display.display();
+  if ((millis() - lastTime) > timerDelay){
+    char datum[200];
+    sprintf(datum, "%s/%s/%i:%i:%i/%i:%i:%i/%i:%i:%i",str[square][chosen],dayStamp,start_hour,start_minute,start_second,new_hor,new_minu,new_sec,hours,minutes,seconds);
+    if(WiFi.status() != WL_CONNECTION_LOST && WiFi.status()== WL_CONNECTED){
+    int x = ThingSpeak.writeField(myChannelNumber, 1,datum, myWriteAPIKey);
+    
+    if(x == 200){
+       display.println("saved!"); 
+       delay(500);}
+    else{
+      
+    }}
+    else{
+      saving+=1;
+      char datum[200];
+      sprintf(datum, "%s/%s/%i:%i:%i/%i:%i:%i/%i:%i:%i",str[square][chosen],dayStamp,start_hour,start_minute,start_second,new_hor,new_minu,new_sec,hours,minutes,seconds);
+      saved_data[saving]=datum;
+      display.print("saved to var");
+      delay(500);
+      }}
+      lastTime = millis();
+     pressed = -1;
+      }
+  else{
+  display.print("00:00:00");}
+  startTimer = 0;
+
+}
+else if(counter_var==1){
+  display.print(bufffer);
+  startTimer = 1;
+}
+
+else if(counter_var==2){
+  display.print(bufffer);
+  startTimer = 2;
+}
+
+ display.setTextSize(1);
+ display.setCursor(0,20+15);
+ char buffer[40];
+ sprintf(buffer,"%i:%i:%i %s %s %s",hours,minutes,seconds,ampm,day,dayStamp);
+ display.print(buffer);
+ int savings = saving;
+ if(saving>-1 and WiFi.status() != WL_CONNECTION_LOST && WiFi.status()== WL_CONNECTED){
+  display.print("saving");
+      for(int i=0;i<savings +1;){
+        if((millis() - lastTime ) > timerDelay){
+      int x = ThingSpeak.writeField(myChannelNumber,1,saved_data[i], myWriteAPIKey);
+      if(x==200){
+       i++;
+       saving -=1;
+       }
+       else{
+        }
+       lastTime = millis();
+       }
+      }
+      }
+ if(WiFi.status() != WL_CONNECTION_LOST && WiFi.status()== WL_CONNECTED){ 
+  if(lastUpdate+1200000<millis()){
+ timeClient.update();
+ cf_data();
+ lastUpdate = millis();
+ }
+ else if(millis()<120000){
+  timeClient.update();
+  if(millis() > 60000){
+  cf_data();}
+  }
+  }}
+
+
+
+
+void cf_div_screen(String upcoming,String contest_date,String ratingAndProblems , int hor , int minu , int sec){
+display.setCursor(30,1);
+display.setTextSize(2);
+display.print(upcoming);
+display.setTextSize(1);
+display.setCursor(20,20);
+display.print(contest_date);
+display.setTextSize(2);
+display.setCursor(10,35);
+display.print("00:00:--");
+display.setTextSize(1);
+display.setCursor(0,56);
+display.print("rating: 837 || x left");
+}  
+
+
+
+
+
+
+void IRAM_ATTR onTimer(){
+  if(startTimer == 1){
+  sec+=1;
+  if(sec>=59){
+    sec=0;
+    minu++;
+    }
+  if(minu>=59){
+    minu=0;
+    hor++;
+    } }
+   else if(startTimer ==0){
+   sec = 0;
+   hor =0;
+   minu = 0;
+   } 
+   beautiful_int(sec,hor,minu);
+ } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void setup() {
+    Serial.begin(9600);
+    
+if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+   
+  }
+  int connection_begin = millis();
+     if(WiFi.status() != WL_CONNECTED){
+      while(WiFi.status() != WL_CONNECTED && millis() <= connection_begin+20000){
+        WiFi.begin(ssid, password);  
+        break;    
+      } 
+     
+    }
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextSize(0);
+  display.setTextColor(WHITE);
+  timeClient.begin();
+  timer = timerBegin(0, 80, true);  // timer 0, MWDT clock period = 12.5 ns * TIMGn_Tx_WDT_CLK_PRESCALE -> 12.5 ns * 80 -> 1000 ns = 1 us, countUp
+  timerAttachInterrupt(timer, &onTimer, true); // edge (not level) triggered 
+  timerAlarmWrite(timer, 1000000, true); // 1000000 * 1 us = 1 s, autoreload true
+  timerAlarmEnable(timer); // enable
+  ThingSpeak.begin(clientt);
+  web_server();
+  pinMode(19,OUTPUT); 
+  pinMode(27,OUTPUT);  
+  irrecv.enableIRIn();
+  irrecv.blink13(true);
+}
+void loop() { 
+char key = keypad.getKey();  
+if(key){
+  display.ssd1306_command(SSD1306_DISPLAYON);
+  lastPress = millis();
+  }  
+if(lastPress+30000<millis()){
+  display.ssd1306_command(SSD1306_DISPLAYOFF);
+  } 
+if(key == 'C'){
+  if(square==activities-1){
+      square = 0;}
+  else{
+    square+=1;
+    }
+    if(pressA == 1){
+     display.clearDisplay();
+    print_label(square);
+    pressA = 1;
+    key = '?';
+    }}   
+if(key=='A' and pressA!=1 and pressD!=1) {
+  int lastSquare = square;
+  display.clearDisplay();
+  print_label(square);
+  pressA=1;
+  key = '?';
+}
+else if(key=='A' and pressA==1){
+display.clearDisplay();
+display.display();
+pressA=0;}
+if(pressA==1){
+edit_key(key);
+if(key_num != -48){
+    chosen = key_num-1;
+  }} 
+ if(key=='D' and pressD!=1 and pressA!=1) {
+ display.clearDisplay();
+cf_div_screen("Div 3","12/june 05:02" , "rating: 837 || 5 left" , hor , minu , sec);
+display.display();
+pressD=1;
+ } 
+else if(key=='D' and pressD==1){
+display.clearDisplay();
+display.display();
+pressD=0;
+
+} 
+if(pressA!=1 and pressD!=1){
+    main_screen(chosen,hor,minu,sec,key,new_hor,new_minu,new_sec);
+   drawer();  
+ }
+get_ir_data();
+
+
+
+display.display();
 }
